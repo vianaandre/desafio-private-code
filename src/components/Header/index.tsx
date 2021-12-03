6227d893493a2bb729e6bfe09db90a655033da47
vianaandre <andredapperviana@gmail.com>
2021-12-03 14:35:10 -0300

    feat: (add menu burguer)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import React, { useEffect, useState } from 'react';
 import { HeaderContainer } from './styled';
 import { ReactComponent as Logo } from '../../assets/logoipsum.svg';
 import { Nav } from '../Nav';
-import { ReactComponent as Hamburguer } from '../../assets/hamburguer.svg';
+import { Container } from '../../styles/container';
 
 export const Header: React.FC = () => {
+  const [menuActive, setMenuActive] = useState<boolean>(false);
+
+  const handleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
+  useEffect(() => {
+    const elementBody = document.querySelector('body');
+
+    if (elementBody) {
+      if (menuActive) {
+        elementBody.style.height = '100vh';
+        elementBody.style.overflow = 'hidden';
+      } else {
+        elementBody.style.height = 'fit-content';
+        elementBody.style.overflowY = 'visible';
+      }
+    }
+  });
+
   return (
-    <HeaderContainer id="header">
-      <Logo />
-      <div className="header-desktop">
-        <Nav />
-      </div>
-      <div className="header-mobile">
-        <Hamburguer />
-        <Menu right>
+    <Container>
+      <HeaderContainer id="header" active={menuActive}>
+        <Logo />
+        <div className="header-desktop">
           <Nav />
-        </Menu>
-      </div>
-    </HeaderContainer>
+        </div>
+        <div className={`${menuActive ? 'active' : ''} header-mobile`}>
+          <div className="header-hamburguer" onClick={handleMenu}>
+            <div className={menuActive ? 'activeTop' : ''} />
+            <div className={menuActive ? 'activeMidlle' : ''} />
+            <div className={menuActive ? 'activeBottom' : ''} />
+          </div>
+          <Nav />
+        </div>
+      </HeaderContainer>
+    </Container>
   );
 };
--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.header`
-    width: 1220px;
+export const HeaderContainer = styled.header<{
+    active: boolean
+}>`
     height: 80px;
     margin: 0 auto;
     display: flex;
@@ -16,31 +17,125 @@ export const HeaderContainer = styled.header`
         display: none;
     }
 
-    @media(min-width: 998px) and (max-width: 1219px) {
-        width: 998px;
-    }
-
-    @media(min-width: 768px) and (max-width: 997px) {
-        width: 768px;
-    }
-
     @media(max-width: 767px) {
-        width: 320px;
-        overflow: hidden;
-
-        .header-desktop {
-            display: none;
-        }
 
         .header-mobile {
             display: block;
+            display: flex;
 
-            svg {
-                float: right;
-                text-align: right;
-                width: 20px;
+            .header-hamburguer {
+
+                position: relative;
+                width: fit-content; 
+                display: flex;
+                flex-direction: column;
+                height: 22px;
+                z-index: 5000;
+                cursor: pointer;
+
+                div {
+                    position: absolute;
+                    right: 0;
+                    width: 30px;
+                    height: 4px;
+                    background-color: ${({ theme }) => { return theme.COLORS.BLACK; }};
+                    display: block;
+                    background-color: ${({ theme }) => { return theme.COLORS.PRIMARY; }};
+                }
+
+                @keyframes menuTop {
+                    from {
+                        top: 9px;
+                    }
+
+                    to {
+                        top: 0
+                    }
+                }
+
+                @keyframes menuBottom {
+                    from {
+                        bottom: 9px;
+                    }
+
+                    to {
+                        bottom: 0
+                    }
+                }
+
+                div:nth-child(1) {
+                    animation: menuTop 0.2s;
+                    animation-fill-mode: forwards;                    
+                }
+
+                div:nth-child(2) {
+                    top: 9px;
+                    transition: 0.2s;
+                }
+
+                div:nth-child(3) {
+                    animation: menuBottom 0.2s;
+                    animation-fill-mode: forwards;
+                }
+
+                @keyframes topArrow {
+
+                    from {
+                        top: 0;
+                    }
+
+                    to {
+                        top: 9px;
+                    }
+                }
+                @keyframes bottomArrow {
+                    from {
+                        bottom: 0;
+                    }
+
+                    to {
+                        bottom: 9px
+                    }
+                }
+
+                div.activeMidlle {
+                    
+                }
+
+                div.activeTop {
+                    position: absolute;
+                    animation: topArrow 0.2s;
+                    animation-fill-mode: forwards;
+                }
+
+                div.activeBottom {
+                    position: absolute;
+                    animation: bottomArrow 0.2s;
+                    animation-fill-mode: forwards;
+                }
+            }
+
+            ul {
+                width: 100%;
+                padding: 100px;
+                height: 100vh;
+                background-color: ${({ theme }) => { return theme.COLORS.BRANCO; }};
+                flex-direction: column;
+                align-items: center;
+                justify-content: space-around;
+                position: fixed;
+                z-index: 2000;
+                right: ${({ active }) => { return (active ? 0 : '-100%'); }};
+                transition: 300ms;
+                top: 0;
+
             }
         }
+
+        .header-desktop {
+            display: none;
+        } 
+
     }
 
     svg {
